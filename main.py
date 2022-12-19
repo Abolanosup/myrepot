86a2e5ba8b383d198a159b84da8fc813c2885760
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-12-18 21:45:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['Abolanos3',] #Poner aqui los user con acceso permanente
+tl_admin_users = ['Abolanos3','elnietodecacha'] #Poner aqui los user con acceso permanente
 godlist = ['Abolanos3'] #Poner aqui los admin 
 
 async def get_root(username):
